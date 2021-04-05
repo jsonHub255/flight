8f9dcd65a9a0c3450b443b17dfcb5c15d92d4655
Json <jsonlli255@gmail.com>
2021-04-05 22:24:32 +0100

    modify admin layout.

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Airport, Flight, Pessenger
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Pessenger)
