e4aa4f1580e094065fca0c8a8843004e1cf0dd46
Json <jsonlli255@gmail.com>
2021-04-05 20:55:51 +0100

    add 'flight booking' option.

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,8 @@
+import flights
 from django.shortcuts import render
-from .models import Flight
+from .models import Airport, Flight, Pessenger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -12,5 +15,13 @@ def flight(request,flight_id):
     flight = Flight.objects.get(pk=flight_id) 
     return render(request, "flights/flights.html", {
         "flight": flight,
-        "pessengers": flight.pessengers.all()
+        "pessengers": flight.pessengers.all(),
+        "non_pessengers": Pessenger.objects.exclude(flights=flight).all()
     })
+    
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        pessenger = Pessenger.objects.get(pk=int(request.POST['pessenger']))
+        pessenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
